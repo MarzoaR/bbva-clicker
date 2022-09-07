a12b1d500e351fa1c14fe2700f990fd27ccb76b8
Roberto Marzoa <roberto.marzoa@insoftar.com>
2022-09-07 15:58:35 +0100

    Se agrega el service worker, pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { FooterModule } from './footer/footer.module';
 import { HeaderModule } from './header/header.module';
 import { VistasModule } from './vistas/vistas.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,13 @@ import { VistasModule } from './vistas/vistas.module';
     VistasModule,
     FooterModule,
     HeaderModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
