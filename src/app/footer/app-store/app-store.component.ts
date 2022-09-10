d7ece488e30a7b4ba78446e238f1ec4bce260a0d
Roberto Marzoa <roberto.marzoa@insoftar.com>
2022-09-10 21:14:31 +0100

    Fix/ se corrige ruta de algunas imagenes

--- src/app/footer/app-store/app-store.component.ts
+++ src/app/footer/app-store/app-store.component.ts
@@ -10,17 +10,17 @@ export class AppStoreComponent implements OnInit {
   stores:any[] = [
     {
       alt: 'App Store',
-      img: '../../assets/app_store/ios_es.png',
+      img: './assets/app_store/ios_es.png',
       url: 'https://itunes.apple.com/es/app/bbva-espana/id325813155?mt=8'
     },
     {
       alt: 'Google Play',
-      img: '../../assets/app_store/android_es.png',
+      img: './assets/app_store/android_es.png',
       url: 'https://play.google.com/store/apps/details?id=com.bbva.bbvacontigo&hl=es'
     },
     {
       alt: 'App Gallery',
-      img: '../../assets/app_store/hos_es.png',
+      img: './assets/app_store/hos_es.png',
       url: 'https://appgallery.huawei.com/#/app/C101673739'
     }
   ]
